309ab5a4acd3658e52962cd6826ff0519c7bae3b
Kaikai8888 <wuky812@gmail.com>
2020-12-29 17:36:34 +0800

    feat: add front end domain to cors white list

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function (err, req, res, next) {
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:8080',
+    origin: ['https://sean-yu-pohsiang.github.io/simple-twitter-frontend-2020', 'http://localhost:8080'],
     methods: ["GET", "POST"],
   }
 })
