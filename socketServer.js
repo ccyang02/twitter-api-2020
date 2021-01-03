76486cea51c4b3805db6c7508b7cd3cece3d8e92
ccyang02 <chiachingyang02@gmail.com>
2021-01-03 23:05:21 +0800

    feat: add backend socket server

---
+++ socketServer.js
@@ -0,0 +1,20 @@
+const passport = require('./config/passport')
+const db = require('./models')
+
+function authenticated(socket, next) {
+  passport.authenticate('jwt', { session: false }, (error, user, info) => {
+    if (error) return next(error)
+    if (!user) return next(new Error('未被授權'))
+    if (user.role === 'admin') return next(new Error('未被授權'))
+    socket.request.user = user
+    return next()
+  })(socket.request, {}, next)
+}
+
+module.exports = async (io) => {
+  io.use(authenticated)
+  io.on('connection', async (socket) => {
+    const token = socket.handshake.auth.token
+    console.log(`a user connected: ${token}`)
+  })
+}
