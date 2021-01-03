963b95b89f7ca6abd6ac4a22720017bb96a0ef65
ccyang02 <chiachingyang02@gmail.com>
2021-01-03 23:23:21 +0800

    wip: add event for frontend testing

--- socketServer.js
+++ socketServer.js
@@ -16,5 +16,9 @@ module.exports = async (io) => {
   io.on('connection', async (socket) => {
     const token = socket.handshake.auth.token
     console.log(`a user connected: ${token}`)
+
+    io.on('test-message', (username) => {
+      console.log('>>>>>>>> This is username from frontend.')
+    })
   })
 }
