4fa858677093385f0d0f4e059de81e5751d28abb
ccyang02 <chiachingyang02@gmail.com>
2021-01-07 23:53:44 +0800

    wip: url

--- socketServer.js
+++ socketServer.js
@@ -14,7 +14,7 @@ const db = require('./models')
 module.exports = async (io) => {
   // io.use(authenticated)
   io.on('connection', async (socket) => {
-    const fakeReq = { headers: { Authorization: `Bearer ${socket.handshake.auth.token}` } }
+    const fakeReq = { headers: { Authorization: `Bearer ${socket.handshake.auth.token}` }, url: 'http://github.com' }
 
     console.log('This is fake request!', fakeReq)
     console.log(socket)
