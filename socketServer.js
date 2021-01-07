003472e8131bab34251b7146b4555ea0c139ef45
ccyang02 <chiachingyang02@gmail.com>
2021-01-07 21:55:08 +0800

    wip: add disconnect event

--- socketServer.js
+++ socketServer.js
@@ -15,10 +15,15 @@ module.exports = async (io) => {
   // io.use(authenticated)
   io.on('connection', async (socket) => {
     const token = socket.handshake.auth.token
+    console.log(`Get socket ${socket.id}`)
     console.log(`a user connected: ${token}`)
 
     socket.on('test-message', (username) => {
       console.log(`>>>>>>>> This is username from frontend. ${username}`)
+    })
+
+    socket.on('disconnect', async () => {
+      console.log(`======> Detect disconnect: ${socket.id}`)
     })
   })
 }
