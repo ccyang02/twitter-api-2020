cec47449fe40fc93a721093dad5cfa90cd731de6
ccyang02 <chiachingyang02@gmail.com>
2021-01-08 01:09:08 +0800

    wip

--- socketServer.js
+++ socketServer.js
@@ -1,76 +1,48 @@
 const passport = require('./config/passport')
 const db = require('./models')
 
-// function authenticated(socket, next) {
-//   passport.authenticate('jwt', { session: false }, (error, user, info) => {
-//     if (error) return next(error)
-//     if (!user) return next(new Error('未被授權'))
-//     if (user.role === 'admin') return next(new Error('未被授權'))
-//     socket.request.user = user
-//     return next()
-//   })(socket.request, {}, next)
-// }
-
-function promisedPassportAuthentication(fakeReq) {
+function promisedVerifyToken(fakeReq) {
   return new Promise((resolve, reject) => {
     passport.authenticate('jwt', { session: false }, (error, user, info) => {
-      if (!user) {
-        reject('you are not the guy!')
-      }
-      if (user) {
-        resolve(user)
-      }
+      if (error) reject({ status: 'error', message: `Passport internal error: ${error}` })
+      if (!user) reject({ status: 'fail', message: 'Authentication fail.' })
+      if (user) resolve({ status: 'success', message: user })
     })(fakeReq, {})
   })
 }
 
 module.exports = async (io) => {
-  // io.use(authenticated)
-
   io.on('connection', async (socket) => {
-    const fakeReq = { headers: { authorization: `Bearer ${socket.handshake.auth.token}` }, url: 'https://sean-yu-pohsiang.github.io/simple-twitter-frontend-2020' }
-
-    console.log('This is fake request!', fakeReq)
-    console.log(socket)
-    console.log('==============================')
-    // let auser;
-    // try {
-    //   await passport.authenticate('jwt', { session: false }, (error, user, info) => {
-    //     // if (error) return next(error)
-    //     if (error) console.log('======> Error!!!!', error)
-    //     if (!user) {
-    //       console.log('Disconnect this socket: ', socket.id)
-    //       socket.disconnect(true)
-    //     }
-    //     if (user) console.log('======= I got u!!', user)
-    //     socket.user = user
-    //     auser = user
-    //   })(fakeReq, {})
-    // } catch (error) {
-    //   console.log(`>>>> passport error: `, error)
-    // }
-    // console.log('123: ', auser)
-    // console.log(`Wanna get user!! ${socket.user}`)
-
-    let getsomeuser;
-    try {
-      getsomeuser = await promisedPassportAuthentication(fakeReq)
-    } catch (error) {
-      console.log('Bad things happen :(', error)
+    const fakeReq = {
+      headers: {
+        authorization: `Bearer ${socket.handshake.auth.token}`
+      },
+      url: 'https://sean-yu-pohsiang.github.io/simple-twitter-frontend-2020'
     }
-    console.log('Get promise!!!!!', getsomeuser)
 
+    let authUser;
+    try {
+      authUser = await promisedVerifyToken(fakeReq)
+    } catch (error) {
+      console.log(error)
+    }
 
-    // const token = socket.handshake.auth.token
-    // console.log(`Get socket ${socket.id}`)
-    // console.log(`a user connected: ${token}`)
+    if (authUser.status === 'success') {
+      socket.user = authUser.message
+    } else {
+      socket.emit('unauthorized', `unauthorized: ${authUser.message}`)
+      socket.disconnect(true)
+    }
+
+    console.log(`Get socket ${socket.user}`)
+    console.log(`a user connected: ${token}`)
 
     socket.on('test-message', (username) => {
-      console.log(`>>>>>>>> This is username from frontend. ${username}`)
+      console.log(`>>> This is username from frontend. ${username}`)
     })
 
     socket.on('disconnect', async () => {
-      console.log(`======> Detect disconnect: ${socket.id}`)
+      console.log(`===> Detect disconnect: ${socket.id}`)
     })
   })
 }
