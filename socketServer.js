82da337991e7fc6d3b8c62783d0c0187a3350fca
ccyang02 <chiachingyang02@gmail.com>
2021-01-08 21:32:57 +0800

    fix: error status logic

--- socketServer.js
+++ socketServer.js
@@ -22,20 +22,24 @@ module.exports = async (io) => {
     let authUser;
     try {
       authUser = await promisedVerifyToken(fakeReq)
+      if (authUser.status === 'success') {
+        socket.user = authUser.message
+        // console.log(`Get socket ${socket.user.name}`) // id name account avatar
+      } else {
+        socket.emit('unauthorized', `unauthorized`)
+        socket.disconnect(true)
+      }
     } catch (error) {
       console.log(error)
-    }
-
-    if (authUser && authUser.status === 'success') {
-      socket.user = authUser.message
-      // console.log(`Get socket ${socket.user.name}`) // id name account avatar
-    } else {
-      socket.emit('unauthorized', `unauthorized`)
-      socket.disconnect(true)
+      throw Error(error)
     }
 
     socket.on('test-message', (username) => {
       console.log(`>>>>>>>> This is username from frontend. ${username}`)
+    })
+
+    socket.on('disconnect', async () => {
+      console.log('Get disconnected user.')
     })
   })
 }
