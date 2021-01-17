96400284ae04a6aff285a9c249e6a38faa1e8388
ccyang02 <chiachingyang02@gmail.com>
2021-01-17 21:17:48 +0800

    wip

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
     origin: ['https://sean-yu-pohsiang.github.io/simple-twitter-frontend-2020', 'http://localhost:8080'],
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
   }
 })
 require('./socketServer')(io)
