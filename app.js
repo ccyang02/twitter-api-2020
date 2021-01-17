683296fd0b7fe3392e06c809d1a5b7f4e8a80c8f
ccyang02 <chiachingyang02@gmail.com>
2021-01-17 21:26:53 +0800

    wip

--- app.js
+++ app.js
@@ -47,9 +47,7 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
     origin: ['https://sean-yu-pohsiang.github.io', 'http://localhost:8080'],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
+    methods: ["GET", "POST"]
   }
 })
 require('./socketServer')(io)
