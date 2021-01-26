e5c217e01aa380599424bc30a4fe182075f5fe97
ccyang02 <chiachingyang02@gmail.com>
2021-01-26 20:48:39 +0800

    fix: frontend backend socket connection problem

--- controllers/socket/public.js
+++ controllers/socket/public.js
@@ -4,7 +4,6 @@ const userSelectedFields = ['id', 'account', 'name', 'avatar']
 
 async function getConnectedUsers(io, onlineUsers) {
   try {
-    console.log('>>>>> onlineUsers: ', onlineUsers)
     const connectedUserIds = Object.keys(onlineUsers).map(Number)
     const connectedUsers = await User.findAll({
       where: { id: { [Op.in]: connectedUserIds } },
@@ -16,7 +15,6 @@ async function getConnectedUsers(io, onlineUsers) {
       user.sckId = onlineUsers[user.id]
     })
 
-    console.log('>>>> connectedUsers: ', connectedUsers)
     await io.emit('init-public', connectedUsers)
   } catch (error) {
     console.log(error)
