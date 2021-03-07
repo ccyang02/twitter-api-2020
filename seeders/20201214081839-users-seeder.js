15a4266ff3567dd422b9c4407ebe6b4d7c5578d2
kellywu888 <kelly.wu@aimazing.co>
2021-03-07 10:08:08 +0800

    fix: make seed users' avatars and covers fix

--- seeders/20201214081839-users-seeder.js
+++ seeders/20201214081839-users-seeder.js
@@ -1,6 +1,7 @@
 'use strict';
 const faker = require('faker')
 const bcrypt = require('bcryptjs')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     try {
@@ -20,8 +21,8 @@ module.exports = {
         account: `User${i + 1}`,
         email: `user${i + 1}@example.com`,
         password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        cover: 'https://loremflickr.com/598/200/landscape/?random=' + Math.floor(Math.random() * 100),
-        avatar: 'https://loremflickr.com/140/140/people/?random=' + Math.floor(Math.random() * 100),
+        cover: 'https://loremflickr.com/598/200/landscape/?lock=' + Math.floor(Math.random() * 100),
+        avatar: 'https://loremflickr.com/140/140/people/?lock=' + Math.floor(Math.random() * 100),
         introduction: faker.lorem.text(),
         createdAt: new Date(2020, 1, 1),
         updatedAt: new Date(2020, 1, 1)
